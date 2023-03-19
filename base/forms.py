7ad40c44dabaab31ab80ec859bcf7aae8a3fbd35
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 20:36:35 +0200

    Editing

--- base/forms.py
+++ base/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 from .models import Post
 
 
@@ -6,3 +7,9 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = '__all__'
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
