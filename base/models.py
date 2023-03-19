4b2ce1dac3c9b235a9cb36941ae9c6fc24d86bd6
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 14:16:02 +0200

    Adding class for sorting data from newest to oldest post

--- base/models.py
+++ base/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-
 class Post(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     image = models.ImageField(upload_to='post-image/', blank=True, null=True)
@@ -13,11 +12,15 @@ class Post(models.Model):
     update = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-update', '-created']
+
     def __str__(self):
         return str(self.host)
 
+
 class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     body = models.TextField()
     update = models.DateTimeField(auto_now=True)
