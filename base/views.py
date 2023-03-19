0cdfa60225998290694dd12be101e54e3eb9a548
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 14:51:31 +0200

    Creating search functionality using Q from django

--- base/views.py
+++ base/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .forms import PostForm
 from .models import Post
 # Create your views here.
 
 
 def home(request):
-    posts = Post.objects.all()
-    context = {'posts': posts}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    posts = Post.objects.filter(
+        Q(description__icontains=q)
+    )
+
+    post_count = posts.count()
+
+    context = {'posts': posts, 'post_count': post_count}
     return render(request, 'base/home.html', context)
+
 
 def post(request, pk):
     post = Post.objects.get(id=pk)
     context = {'post': post}
     return render(request, 'base/post.html', context)
+
 
 def createPost(request):
     form = PostForm()
@@ -25,6 +34,7 @@ def createPost(request):
     context = {'form': form}
     return render(request, 'base/post_form.html', context)
 
+
 def updatePost(request, pk):
     post = Post.objects.get(id=pk)
     form = PostForm(instance=post)
@@ -36,6 +46,7 @@ def updatePost(request, pk):
 
     context = {'form': form}
     return render(request, 'base/post_form.html', context)
+
 
 def delete(request, pk):
     post = Post.objects.get(id=pk)
