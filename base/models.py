41125cd975041573db4f043018c7e0ca8c77e180
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 13:03:55 +0200

    Crating Topic model and Comment Model

--- base/models.py
+++ base/models.py
@@ -3,10 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
-    # host =
-    # topic =
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True, blank=True)
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='post-image/', blank=True, null=True)
     description = models.TextField(null=True, blank=True)
@@ -16,3 +21,13 @@ class Post(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) 
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField()
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.body[0:50])
