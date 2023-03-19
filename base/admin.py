c62d7568b8c592d32d8eba242ab8ee68399af6be
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 13:04:47 +0200

    Registering models to the admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post, Topic, Comment
 
 # Register your models here.
+
+admin.site.register(Post)
+admin.site.register(Topic)
+admin.site.register(Comment)
