0ebf0edf938580de21eb8eefd0b0854b6aadb256
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 17:11:56 +0200

    Editing site registration

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Post, Comment
+from .models import Post, Message
 
 # Register your models here.
 
 admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Message)
