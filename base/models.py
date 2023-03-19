341c113b67cc76126b1aff703e4a4619d3dabc43
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 12:46:12 +0200

    Creating first Model (Post model)

--- base/models.py
+++ base/models.py
@@ -4,3 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Post(models.Model):
+    # host =
+    # topic =
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='post-image/', blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    update = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.name)
