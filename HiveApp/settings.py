f26f91659b616acf7475a85233421239f3b9b52b
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 21:19:42 +0200

    AUTH_USER_MODEL

--- HiveApp/settings.py
+++ HiveApp/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
 ]
+
+AUTH_USER_MODEL = 'base.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
