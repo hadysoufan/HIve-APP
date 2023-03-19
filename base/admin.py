7a93e691eae9c2106a823b20376427715ccbc9bd
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 13:13:16 +0200

    Editing

--- base/admin.py
+++ base/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Post, Topic, Comment
+from .models import Post, Comment
 
 # Register your models here.
 
 admin.site.register(Post)
-admin.site.register(Topic)
 admin.site.register(Comment)
--- base/models.py
+++ base/models.py
@@ -3,16 +3,10 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class Topic(models.Model):
-    name = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.name
 
 class Post(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True, blank=True)
-    name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='post-image/', blank=True, null=True)
     description = models.TextField(null=True, blank=True)
     # participants =
@@ -20,7 +14,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.name)
+        return str(self.host)
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) 
