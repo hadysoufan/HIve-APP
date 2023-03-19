07d0a334f077f58c2533cb1784b50b214dfb4257
hadysoufan <hadisoufan74@gmail.com>
2023-03-19 13:03:41 +0200

    Editing post view to retrieve data from DB

--- base/views.py
+++ base/views.py
@@ -1,26 +1,15 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
-
-posts = [
-    {'id': 1, 'username': 'hadisoufan', 'caption': 'Hello Classmates'},
-    {'id': 2, 'username': 'cleopatra',
-        'caption': 'Just started a new chapter in my book'},
-    {'id': 3, 'username': 'melia', 'caption': 'Check out my recent certificate'},
-    {'id': 4, 'username': 'zayn', 'caption': 'happy b-day hadi'},
-]
 
 
 def home(request):
-
+    posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'base/home.html', context)
 
 
 def post(request, pk):
-    post = None
-    for i in posts:
-        if i['id'] == int(pk):
-            post = i
+    post = Post.objects.get(id=pk)
     context = {'post': post}
     return render(request, 'base/post.html', context)
