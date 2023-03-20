7a5433734ce488f43c4e0e3caa33b9a6c005810a
hadysoufan <hadisoufan74@gmail.com>
2023-03-20 09:56:05 +0200

    migrating

--- base/models.py
+++ base/models.py
@@ -3,16 +3,24 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(AbstractUser):
-    pass
 
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(
+        upload_to="profile-image/", null=True, default="default.png")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
 
 class Post(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     image = models.ImageField(upload_to='post-image/', blank=True, null=True)
-    description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User, related_name='participants' ,blank=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
     update = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
